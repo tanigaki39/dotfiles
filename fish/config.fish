c56713c74c0c920bb9f33d469c40cad1acf18c87
tanigaki <junya.tanigaki.39@gmail.com>
2019-12-05 17:58:52 +0900

    Add function for plugin-peco

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,10 @@ function cd
   builtin cd $argv; and ls
 end
 
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
+
 # appearance
 set -g theme_display_date no
 set -g theme_color_scheme dracula
